28f6a406b4cfc901fa2571636a0d8683d829c17c
delm83 <delm83@googlemail.com>
2024-08-05 10:41:30 +0100

    retrieve stock data from freecodecamp stock price checker proxy

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,11 @@
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res){
+      let stock = req.query.stock
+      let like = req.query.like
+      let response = await fetch('https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/'+stock+'/quote')
+      let data = await response.json()
+      res.json({"stockData":{"stock":data.symbol, "price":data.latestPrice}})
     });
-    
 };

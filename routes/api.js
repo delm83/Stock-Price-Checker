6d838191041bd980f73584cf7c2acbf3e1642b4d
Derek Mudie <delm83@googlemail.com>
2024-08-06 18:35:58 +0000

    handle adding new stock and ip address to database

--- routes/api.js
+++ routes/api.js
@@ -7,12 +7,43 @@ module.exports = function (app) {
 
   let Schema = mongoose.Schema;
 
+  let stockSchema = new Schema({
+     stockname: String,
+     ipLikedList: [String],
+     likes: Number
+ });
+
+ let Stock = mongoose.model("Stock", stockSchema)
+
   app.route('/api/stock-prices')
     .get(async function (req, res){
-      let stock = req.query.stock
+      try{
+      let stockname = req.query.stock
       let like = req.query.like
-      let response = await fetch('https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/'+stock+'/quote')
+      let ip = req.ip
+      let response = await fetch('https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/'+stockname+'/quote')
       let data = await response.json()
-      res.json({"stockData":{"stock":data.symbol, "price":data.latestPrice}})
-    });
+      console.log(data.symbol)
+      let inputStock = await Stock.findOne({stockname: data.symbol})
+      console.log(inputStock)
+      console.log(like)
+      if(inputStock){
+        console.log('exists')
+      return res.json({"stockData":{"stock":data.symbol, "price":data.latestPrice, "likes":inputStock.likes}})
+      }
+      else{
+      if(like === "true"){newLikeHandler(data.symbol, ip)}
+      }
+      res.json({"stockData":{"stock":data.symbol, "price":data.latestPrice, "likes":like === "true"?1:0}})
+    }catch(err){return res.json({error: err})}
+     });
+
+      async function newLikeHandler(stockname, ip){
+        let newStock = Stock({
+          stockname: stockname,
+          ipLikedList: [ip],
+          likes: 1
+      });
+         await newStock.save();
+      }
 };
